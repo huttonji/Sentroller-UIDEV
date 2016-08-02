782b7e8e87c3f6b2de5e4623e5a0935ca67cb3d7
jamesHutton <james@braingrid.org>
2016-08-02 19:17:10 -0400

    x

---
+++ BrainGrid README.txt
@@ -0,0 +1,6 @@
+-Visual Studio 2013
+-Xamarin installed in C:\Program Files (x86)\Microsoft Visual Studio 12.0\Xamarin
+-GITHUB repo (Sentroller-UIDEV) installed in C:\Users\<user>\Documents\GitHub
+-Instaled Java 1.6
+-Installed Java 1.7 (MUST BE 32bit!)
+-Installed Android SDK
--- UIDEV/UIDEV/Form1.vb
+++ UIDEV/UIDEV/Form1.vb
@@ -5,10 +5,16 @@ Imports System.ComponentModel
 
 
 
+
+
+
+
+
 Public Class Form1
 
-    Dim availablePorts As Array
 
+    Dim availablePorts As Array
+    Dim portButton(availablePorts.Length) As System.Windows.Forms.Button
 
     'Dim portButton As New System.Windows.Forms.Button()  'adds a new instance of the class "button" (like a prefix)
 
@@ -21,12 +27,15 @@ Public Class Form1
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.BackgroundImage = My.Resources.choose_port
 
-        'load serial ports (make this on a timer?)
-        availablePorts = IO.Ports.SerialPort.GetPortNames()
-        Dim portButton(availablePorts.Length) As System.Windows.Forms.Button
-        'generate COM port buttons
+        If (SerialPort1.IsOpen()) Then SerialPort1.Close() 'make sure port is closed on startup
+
+        Me.BackgroundImage = My.Resources.choose_port                                                                                       'GREY OUT SENTROLLER MORE WHEN NOT CONNECTED...REAL PICTURE OF SENTROLLER?
+
+        'load serial ports                                                                                                                  'REFRESH PORT OPTIONS AUTOMATICALLY
+        availablePorts = IO.Ports.SerialPort.GetPortNames()                                                                                 'INTERRUPT WHEN CONNECTION LOST
+
+        'generate COM port buttons                                                                                                          'AUTOMATICALLY DETERMINE WHICH COM PORT???
         For i As Integer = 0 To availablePorts.Length - 1
             portButton(i) = New System.Windows.Forms.Button
             portButton(i).Text = availablePorts(i)
@@ -53,13 +62,45 @@ Public Class Form1
         Next i
 
 
-       
+
+
 
 
 
 
     End Sub
 
+    Private Sub portButton_click(Sender As Object, e As EventArgs)
+        SerialPort1.PortName = Sender.text
+        Try
+            SerialPort1.Open()
+            SerialPort1.WriteLine("ver")
+        Catch ex As Exception
+            Me.BackgroundImage = UIDEV.My.Resources.port_error      'if port error, put up red error, and leave function
+            Return
+        End Try
+
+
+        'If port opened successfully:
+
+        Array.Clear(portButton, 0, portButton.Length)               'clear all other ports and all the buttons
+        Array.Clear(availablePorts, 0, availablePorts.Length)
+
+        Me.BackgroundImage = UIDEV.My.Resources.main                'animate / notify however that connection is live
+
+
+
+
+    End Sub
+
+
+    Private Sub SerialErrorReceivedEventHandler(sender As Object, e As )
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As  )
+
+    End Sub
 
     Private Sub portButton_MouseEnter(Sender As Object, e As EventArgs)
         Sender.backgroundImage = UIDEV.My.Resources.Resources.b_blue_fuzz
