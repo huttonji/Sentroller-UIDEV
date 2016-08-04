4d8880d99a8c96cd1fb97feab1e8133b6d429403
jamesHutton <james@braingrid.org>
2016-08-04 04:50:56 -0400

    AutoDetect sentroller on COM port
    TODO:
    -fix up scrollbar / sizing of feed box
    -interrupt when COM port disconnected
    -appropriate background
    -COM port buttons along top?
    -Version Number goes at top, not in terminal feed

--- UIDEV/UIDEV/Form1.Designer.vb
+++ UIDEV/UIDEV/Form1.Designer.vb
@@ -24,24 +24,57 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.VScrollBar1 = New System.Windows.Forms.VScrollBar()
+        Me.terminalOutput = New System.Windows.Forms.TextBox()
+        Me.sentrollerSearchTimer = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'SerialPort1
         '
+        '
+        'VScrollBar1
+        '
+        Me.VScrollBar1.Location = New System.Drawing.Point(664, 787)
+        Me.VScrollBar1.Name = "VScrollBar1"
+        Me.VScrollBar1.Size = New System.Drawing.Size(17, 80)
+        Me.VScrollBar1.TabIndex = 0
+        '
+        'terminalOutput
+        '
+        Me.terminalOutput.BackColor = System.Drawing.Color.Black
+        Me.terminalOutput.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.terminalOutput.Font = New System.Drawing.Font("Monospac821 BT", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.terminalOutput.ForeColor = System.Drawing.Color.Lime
+        Me.terminalOutput.Location = New System.Drawing.Point(36, 780)
+        Me.terminalOutput.MaximumSize = New System.Drawing.Size(245, 0)
+        Me.terminalOutput.Name = "terminalOutput"
+        Me.terminalOutput.Size = New System.Drawing.Size(245, 15)
+        Me.terminalOutput.TabIndex = 1
+        '
+        'sentrollerSearchTimer
+        '
+        Me.sentrollerSearchTimer.Enabled = True
+        Me.sentrollerSearchTimer.Interval = 200
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.BackgroundImage = Global.UIDEV.My.Resources.Resources.choose_port
+        Me.BackgroundImage = Global.UIDEV.My.Resources.Resources.main
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1417, 911)
+        Me.Controls.Add(Me.terminalOutput)
+        Me.Controls.Add(Me.VScrollBar1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents VScrollBar1 As System.Windows.Forms.VScrollBar
+    Friend WithEvents terminalOutput As System.Windows.Forms.TextBox
+    Friend WithEvents sentrollerSearchTimer As System.Windows.Forms.Timer
 
 End Class
--- UIDEV/UIDEV/Form1.vb
+++ UIDEV/UIDEV/Form1.vb
@@ -12,54 +12,39 @@ Imports System.ComponentModel
 
 Public Class Form1
 
-
+    Dim isConnected As Boolean = False
+    Dim connectAttempt_PortNumber As Integer = 0
     Dim availablePorts As Array
-    Dim portButton(availablePorts.Length) As System.Windows.Forms.Button
 
-    'Dim portButton As New System.Windows.Forms.Button()  'adds a new instance of the class "button" (like a prefix)
 
 
     Delegate Sub setTextCallBack(ByVal [text] As String)  'a delegate points to a function (unknown before runtime)
 
-
-
+    Dim terminalFeed As New TextBox With
+      {.BorderStyle = 0, .Left = 36, .Top = 808, .Width = 320, .Height = 805, .ForeColor = Color.Lime,
+       .BackColor = Color.Black, .Font = New Font("Microsoft Sans Serif", 9), .Cursor = Cursors.Arrow, .Multiline = True, .ScrollBars = ScrollBars.Vertical}
+    'add the box
 
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        If (SerialPort1.IsOpen()) Then SerialPort1.Close() 'make sure port is closed on startup
+        'load initial background
+        Me.BackgroundImage = My.Resources.main                                                                                   'GREY OUT SENTROLLER MORE WHEN NOT CONNECTED...REAL PICTURE OF SENTROLLER?
 
-        Me.BackgroundImage = My.Resources.choose_port                                                                                       'GREY OUT SENTROLLER MORE WHEN NOT CONNECTED...REAL PICTURE OF SENTROLLER?
+
+        'initiate the terminal feed display box
+        Controls.Add(terminalFeed)
 
         'load serial ports                                                                                                                  'REFRESH PORT OPTIONS AUTOMATICALLY
         availablePorts = IO.Ports.SerialPort.GetPortNames()                                                                                 'INTERRUPT WHEN CONNECTION LOST
 
-        'generate COM port buttons                                                                                                          'AUTOMATICALLY DETERMINE WHICH COM PORT???
-        For i As Integer = 0 To availablePorts.Length - 1
-            portButton(i) = New System.Windows.Forms.Button
-            portButton(i).Text = availablePorts(i)
-            portButton(i).BackgroundImage = UIDEV.My.Resources.Resources.b_blue
-            portButton(i).BackgroundImageLayout = ImageLayout.Stretch
-            portButton(i).BackColor = Color.Transparent
-            portButton(i).ForeColor = Color.DeepSkyBlue
-            portButton(i).FlatAppearance.BorderColor = Color.Black
-            portButton(i).FlatStyle = FlatStyle.Flat
-            portButton(i).Cursor = Cursors.Hand
-            portButton(i).Font = New Font("Century Gothic", 11, FontStyle.Bold)
+    End Sub
 
-            portButton(i).Width = 85
-            portButton(i).Height = 46
 
-            'grid placement:
-            'shift every odd button 100px right, and shift each new set of 2 down 60px
-            portButton(i).Location = New Point((1000 + ((i Mod 2) * 100)), (460 + Int(i / 2) * 60))
-
-            AddHandler portButton(i).MouseEnter, AddressOf portButton_MouseEnter
-            AddHandler portButton(i).MouseLeave, AddressOf portButton_MouseLeave
-
-            Controls.Add(portButton(i))
-        Next i
+    Private Sub SerialPort1_DataReceived(sender As Object, ByVal e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ' terminalFeed.Text &= SerialPort1.ReadExisting
+    End Sub
 
 
 
@@ -67,67 +52,48 @@ Public Class Form1
 
 
 
+
+    Private Sub terminalOutput_KeyDown(sender As Object, ByVal e As KeyEventArgs) Handles terminalOutput.KeyDown
+
+        'Send output through serial, add it to the feed, and clear the output box
+        If e.KeyCode = Keys.Return Then
+            SerialPort1.WriteLine(terminalOutput.Text)
+            terminalFeed.Text = terminalOutput.Text & vbNewLine & terminalFeed.Text
+            terminalOutput.Text = ""
+        End If
 
     End Sub
 
-    Private Sub portButton_click(Sender As Object, e As EventArgs)
-        SerialPort1.PortName = Sender.text
+    Private Sub sentrollerSearchTimer_Tick(sender As Object, e As EventArgs) Handles sentrollerSearchTimer.Tick
+
+        If SerialPort1.IsOpen Then
+            If SerialPort1.BytesToRead > 0 Then
+                isConnected = True
+                sentrollerSearchTimer.Enabled = False
+                ' SerialPort1.DiscardOutBuffer()
+                terminalFeed.Text = terminalFeed.Text + SerialPort1.ReadExisting
+                Return
+            End If
+            SerialPort1.Close()
+        End If
         Try
+            SerialPort1.PortName = availablePorts(connectAttempt_PortNumber)
+
             SerialPort1.Open()
             SerialPort1.WriteLine("ver")
         Catch ex As Exception
-            Me.BackgroundImage = UIDEV.My.Resources.port_error      'if port error, put up red error, and leave function
-            Return
         End Try
 
+        'handle next COM port try
+        availablePorts = IO.Ports.SerialPort.GetPortNames()                 'refresh avail ports list
+        connectAttempt_PortNumber = connectAttempt_PortNumber + 1
+        If connectAttempt_PortNumber = availablePorts.Length Then
+            connectAttempt_PortNumber = 0
 
-        'If port opened successfully:
-
-        Array.Clear(portButton, 0, portButton.Length)               'clear all other ports and all the buttons
-        Array.Clear(availablePorts, 0, availablePorts.Length)
-
-        Me.BackgroundImage = UIDEV.My.Resources.main                'animate / notify however that connection is live
-
-
-
-
-    End Sub
-
-
-    Private Sub SerialErrorReceivedEventHandler(sender As Object, e As )
-
-    End Sub
-
-    Private Sub SerialPort1_DataReceived(sender As Object, e As  )
-
-    End Sub
-
-    Private Sub portButton_MouseEnter(Sender As Object, e As EventArgs)
-        Sender.backgroundImage = UIDEV.My.Resources.Resources.b_blue_fuzz
-    End Sub
-
-    Private Sub portButton_MouseLeave(Sender As Object, e As EventArgs)
-        Sender.backgroundImage = UIDEV.My.Resources.Resources.b_blue
-    End Sub
-
-
-    'Incoming Serial handling///////////////////////////////////////////////////
-    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
-        ReceivedText(SerialPort1.ReadExisting)
-    End Sub
-
-    Private Sub ReceivedText(ByVal [text] As String)
-        '  If Me.tb_output.InvokeRequired Then
-        Dim x As New setTextCallBack(AddressOf ReceivedText)
-        Me.Invoke(x, New Object() {(text)})
-        ' Else
-        'Me.tb_output.Text &= [text]
-        ' End If
-    End Sub
+        End If
 
 
 
-    Private Sub Button7_Click(sender As Object, e As EventArgs)
 
     End Sub
 End Class
